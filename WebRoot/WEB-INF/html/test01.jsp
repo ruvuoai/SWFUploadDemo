94a6f5e48797f1ba25fc5006fcdce1b730ac300f
zhuer0632 <zhushaolong@gmail.com>
2012-10-11 21:20:48 +0800

    .
    Signed-off-by: zhuer0632 <zhushaolong@gmail.com>

--- WebRoot/WEB-INF/html/test01.jsp
+++ WebRoot/WEB-INF/html/test01.jsp
@@ -6,34 +6,120 @@
 		<title>jQuery File Upload Example</title>
 		<script src="${path}/js/jquery-1.7.2.min.js"></script>
 		<script src="${path}/js/jquery.ui.widget.js"></script>
-		<script src="${path}/js/jquery.iframe-transport.js"></script>
 		<script src="${path}/js/jquery.fileupload.js"></script>
 		<script type="text/javascript">
-		$(function () {
-		    $('#fileupload').fileupload({
-		        dataType: 'json',
-		        done: function (e, data) {
-		            $.each(data.result, function (index, file) {
-		                $('<p/>').text(file.name).appendTo(document.body);
-		            });
-		        }
-		    });
-		});
-		
-
-//ready over
-
-	
+	$(function() {
+		$('#fileupload').fileupload({
+	        dataType: 'json',
+	        progressall: function (e, data) {
+	        var progress = parseInt(data.loaded / data.total * 100, 10);
+	        $('#progress .bar').css(
+	            'width',
+	            progress + '%'
+	        );
+	       
+        },
+        done: function (e, data) {
+         
+           $.each(data.result, function (index, file) {
+             $('<p/>').text(file.name).appendTo(document.body);
+            });
+           
+	        //if($.browser.msie){
+			//	    var data_to_parse = $( 'pre', data.result ).text();
+			//	}else{
+			//	    var data_to_parse = data.result;
+			//	}
+			//	var result = jQuery.parseJSON(data_to_parse);
+			//	var filename = result[0].name;
+			//    alert(filename);
+		   
+        }
+    });
+	});
+	//ready over
 </script>
+
+<link href="http://blueimp.github.com/jQuery-File-Upload/css/jquery.fileupload-ui.css" rel="stylesheet">
+
+<style type="text/css">
+	.bar {
+	  height: 18px;
+	  background: green;
+	}
+	div{
+		margin: 50px;
+		float: none;
+	}
+	
+	.fileinput-button input {
+    -moz-border-bottom-colors: none;
+    -moz-border-left-colors: none;
+    -moz-border-right-colors: none;
+    -moz-border-top-colors: none;
+    -moz-transform: translate(-300px, 0px) scale(4);
+    border-color: transparent;
+    border-image: none;
+    border-style: solid;
+    border-width: 0 0 100px 200px;
+    cursor: pointer;
+    direction: ltr;
+    margin: 0;
+    opacity: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+}
+
+ 
+.btn {
+
+  border-color: rgba(0, 0, 0, 0.15) rgba(0, 0, 0, 0.15) rgba(0, 0, 0, 0.25);
+
+    -moz-border-bottom-colors: none;
+    -moz-border-left-colors: none;
+    -moz-border-right-colors: none;
+    -moz-border-top-colors: none;
+    background-color: #F5F5F5;
+    background-image: -moz-linear-gradient(center top , #FFFFFF, #E6E6E6);
+    background-repeat: repeat-x;
+    border-color: #E6E6E6 #E6E6E6 #A2A2A2;
+    border-image: none;
+    border-radius: 4px 4px 4px 4px;
+    border-style: solid;
+    border-width: 1px;
+    box-shadow: 0 1px 0 rgba(255, 255, 255, 0.2) inset, 0 1px 2px rgba(0, 0, 0, 0.05);
+    color: #333333;
+    cursor: pointer;
+    display: inline-block;
+    font-size: 14px;
+    line-height: 20px;
+    margin-bottom: 0;
+    padding: 4px 14px;
+    text-align: center;
+    text-shadow: 0 1px 1px rgba(255, 255, 255, 0.75);
+    vertical-align: middle;
+}
+</style>
+
 	</head>
 	<body>
-
-		<input id="fileupload" type="file" name="files[]"
-			data-url="${path}/FileUploadCtrl/doupload.do" multiple>
+		
+		<div>
+			 <span class="btn fileinput-button">
+               <span>Add files...</span>
+               <input id="fileupload" type="file" name="input_file" data-url="${path}/FileUploadCtrl/doupload.do" multiple>
+        	 </span>
+		</div>
+			
+		<div style="clear: both;"></div>	
+		
 		<div id="progress">
 			<div class="bar" style="width: 0%;"></div>
 		</div>
-
-
+		
+		<div id="msg"></div>
+		 
+		
 	</body>
 </html>
--- src/com/me/control/FileUploadCtrl.java
+++ src/com/me/control/FileUploadCtrl.java
@@ -4,8 +4,9 @@ package com.me.control;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,10 +15,11 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.servlet.ModelAndView;
 
+import com.me.domain.UploadedFile;
 import com.me.ut.WebPath;
 
 
@@ -34,36 +36,32 @@ public class FileUploadCtrl
     @RequestMapping("/showFileUploadCtrl")
     public String showFileUploadCtrl()
     {
-        return "test01";
+        return "test02";
     }
 
 
     @RequestMapping(value = "/doupload", method = RequestMethod.POST)
-    // Filedata此处对应js/swfupload/swfupload.js中的this.ensureDefault("file_post_name",
-    // "Filedata");
-    public ModelAndView handleFormUpload(HttpServletResponse response,
-            HttpServletRequest request)
+    public @ResponseBody
+    List<UploadedFile> handleFormUpload(
+                                        @RequestParam("input_file") MultipartFile file,
+                                        HttpServletRequest request,
+                                        HttpServletResponse response)
     {
-
-        ModelAndView mod = new ModelAndView();
-
-        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
-        // 获得文件
-        MultipartFile file = multipartRequest.getFile("files[]");
-        // 获得文件名
         String filename = file.getOriginalFilename();
-        
         OutputStream output = null;
         File outfile = null;
         try
         {
+
             filename = new String(filename.getBytes("iso-8859-1"), "UTF-8");
             String folderpath = WebPath.getUploadRootPath();
             String filePath = folderpath + filename;
             
             outfile = new File(filePath);
             output = new FileOutputStream(outfile);
-            IOUtils.copy(file.getInputStream(), output);
+            IOUtils.copy(
+                    file.getInputStream(),
+                    output);
 
             System.out.println(filename);
         }
@@ -84,14 +82,75 @@ public class FileUploadCtrl
                     ioe.printStackTrace();
                 }
             }
-            
-            // 如果上传过程中出现异常：告知前台错误
-            mod.setViewName("fieldtest");
-            mod.addObject("fielderr", "上传文件" + filename + "的时候出现错误");
         }
-        
-        mod.setViewName("test01");
-        return mod;
 
+        List<UploadedFile> uploadedFiles = new ArrayList<UploadedFile>();
+        UploadedFile u = new UploadedFile(filename, Long.valueOf(
+                file.getSize()).intValue(), filename);
+        uploadedFiles.add(u);
+        // HttpHeaders headers = new HttpHeaders();
+        // MediaType mt = new MediaType("text", "html", Charset.forName("gbk"));
+        // headers.setContentType(mt);
+        // ResponseEntity<String> re = new
+        // ResponseEntity<String>(Json.toJson(uploadedFiles), HttpStatus.OK);
+        return uploadedFiles;
     }
+
+
+    @RequestMapping(value = "/doupload_ie", method = RequestMethod.POST)
+    public @ResponseBody
+    List<UploadedFile> handleFormUpload_ie(
+                                           @RequestParam("input_file") MultipartFile file,
+                                           HttpServletRequest request,
+                                           HttpServletResponse response)
+    {
+        String filename = file.getOriginalFilename();
+        OutputStream output = null;
+        File outfile = null;
+        try
+        {
+
+            filename = new String(filename.getBytes("iso-8859-1"), "UTF-8");
+            String folderpath = WebPath.getUploadRootPath();
+            String filePath = folderpath + filename;
+
+            outfile = new File(filePath);
+            output = new FileOutputStream(outfile);
+            IOUtils.copy(
+                    file.getInputStream(),
+                    output);
+
+            System.out.println(filename);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (output != null)
+            {
+                try
+                {
+                    output.close();
+                }
+                catch (IOException ioe)
+                {
+                    ioe.printStackTrace();
+                }
+            }
+        }
+
+        List<UploadedFile> uploadedFiles = new ArrayList<UploadedFile>();
+        UploadedFile u = new UploadedFile(filename, Long.valueOf(
+                file.getSize()).intValue(), filename);
+        uploadedFiles.add(u);
+        // HttpHeaders headers = new HttpHeaders();
+        // MediaType mt = new MediaType("text", "html", Charset.forName("gbk"));
+        // headers.setContentType(mt);
+        // ResponseEntity<String> re = new
+        // ResponseEntity<String>(Json.toJson(uploadedFiles), HttpStatus.OK);
+        return uploadedFiles;
+    }
+
 }
